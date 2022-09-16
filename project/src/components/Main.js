d71c78cc669ef59d4b0ed639a4f8b2ca8874d3e1
anjali790 <anjaliruby790@gmail.com>
2022-09-16 17:54:28 +0530

    practice

--- project/src/components/Main.js
+++ project/src/components/Main.js
@@ -5,14 +5,14 @@ export class Main extends Component{
         super(props);
     }
     
-    
+
     render(){
         return(
             <>
             <main>
             <p className="para">Para no. {this.props.paraNo} </p>
             <p className="para">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut sed temporibus doloribus quidem sapiente suscipit veritatis dolorum hic nulla! Quia, incidunt. Id quam exercitationem enim rerum, est blanditiis dolorum quae</p>
-            <button className="btn">click here</button>
+            <button className="btn">Click here</button>
             </main>
             </>
         )
