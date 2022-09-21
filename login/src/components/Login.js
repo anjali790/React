397c4436df667e18022bdbc6695b9077a224f51c
anjali790 <anjaliruby790@gmail.com>
2022-09-21 16:24:34 +0530

    login

--- login/src/components/Login.js
+++ login/src/components/Login.js
@@ -14,12 +14,12 @@ export class Login extends Component {
                         <fieldset>
                             <h1>Login Form</h1>
                             <div className="usernameDiv">
-                                <label for="username" >username*</label>
-                                <input type="text" placeholder="enter username" value="" className="inputName" required></input>
+                                <label for="username" className="input">username*</label>
+                                <input type="text" placeholder="enter username" value="" className="inputName input" required></input>
                             </div>
                             <div className="passwordDiv">
-                                <label for="password">password*</label>
-                                <input type="text" placeholder="enter password" value="" className="inputPassword" required></input>
+                                <label for="password" className="input">password*</label>
+                                <input type="text" placeholder="enter password" value="" className="inputPassword input" required></input>
                             </div>
                             <div className="btnDiv">
                                 <button type="submit" value="" className="btn">Login</button>
