30733920f1b6699a6143194e17619cad90ed52a5
anjali790 <anjaliruby790@gmail.com>
2022-09-21 18:02:15 +0530

    instagram

---
+++ instagram/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import './components/Insta.css';
+
+import {InstaHeader} from './components/InstaHeader';
+import {InstaPost} from './components/InstaPost';
+
+function App() {
+  return (
+    <>
+    <InstaHeader/>
+    <InstaPost/>
+    </>
+  );
+}
+
+export default App;
