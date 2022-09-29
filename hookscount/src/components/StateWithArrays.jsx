a11a83576aff55127d8d3af84f4254028fbc2cf7
anjali790 <anjaliruby790@gmail.com>
2022-09-29 15:50:32 +0530

    state with array

--- hookscount/src/components/StateWithArrays.jsx
+++ hookscount/src/components/StateWithArrays.jsx
@@ -1,20 +1,32 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 
 export function StateWithArrays() {
-    const [items, setItems] = useState(['apple', 'banana', 'orange']);
-    const [fruit, setFruit] = useState('tomato');
+  const [items, setItems] = useState([]);
+  const [fruit, setFruit] = useState('');
 
-    let addItem = ()=>{
-       setItems([...items, fruit]);
-    }
-
-
+  const addItem = () => {
+    setItems([...items, fruit]);
+    setFruit('');
+  }
+  
   return (
-   <>
-   <input type="text" value={fruit} onChange={(e)=> setFruit(e.target.value)}></input>
-   <button onClick={addItem}>Add items</button>
-    {items.map(item => <p>{item}</p>)}
-    
-   </>
+    <>
+      <input type="text" placeholder='enter todo here' value={fruit} onChange={(e) => setFruit(e.target.value)}></input>
+      <button onClick={addItem}>Add items</button>
+      {items.map(item =>
+        <>
+          <div>
+            <span>{item}</span>
+            <button>Edit</button>
+            <button onClick={()=>{
+              let filteredItems = items.filter((value) => value !== item);
+              setItems([...filteredItems]);
+            }}>Delete</button>
+          </div>
+        </>
+      )}
+
+
+    </>
   )
 }
