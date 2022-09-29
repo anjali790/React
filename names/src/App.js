befd8e7e2abf350008514b4fadf080a49f11cb6a
anjali790 <anjaliruby790@gmail.com>
2022-09-29 15:50:57 +0530

    names

--- names/src/App.js
+++ names/src/App.js
@@ -4,7 +4,8 @@ import {Names} from './components/Names'
 function App() {
   return (
     <>
-    <Names/>
+    <Names name="priya"><h1>Twinkie</h1><p>This is a para tag</p></Names>
+    {/* <Names/> */}
     </>
   );
 }
