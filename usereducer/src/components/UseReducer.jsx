69a0f08270c41f62cc1dbe8bd8dce60616516ab5
anjali790 <anjaliruby790@gmail.com>
2022-10-04 14:26:15 +0530

    useReducer

--- usereducer/src/components/UseReducer.jsx
+++ usereducer/src/components/UseReducer.jsx
@@ -1,12 +1,17 @@
 import React, { useReducer } from "react";
 
-const initialState = 0;
+// const initialState = 0;
+const initialState = {
+    value: 0,
+};
 const reducer = (state, action) => {
     switch (action) {
         case 'INCREMENT':
-            return state + 1;
+            // return state + 1;
+            return { ...state, value: state.value + 1 };
         case 'DECREMENT':
-            return state - 1;
+            // return state - 1;
+            return { ...state, value: state.value - 1 };
         case 'RESET':
             return initialState;
         default:
@@ -14,11 +19,12 @@ const reducer = (state, action) => {
     }
 }
 export function UseReducer() {
-    const [count, dispatch] = useReducer(reducer,initialState);
+    // const [count, dispatch] = useReducer(reducer,initialState);
+    const [countObj, dispatch] = useReducer(reducer, initialState);
 
     return (
         <>
-            <h1>count - {count}</h1>
+            <h1>count - {countObj.value}</h1>
             <button onClick={() => dispatch('INCREMENT')}>Increment</button>
             <button onClick={() => dispatch('DECREMENT')}>Decrement</button>
             <button onClick={() => dispatch('RESET')}>Reset</button>
