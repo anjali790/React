f04885d3d6160b93bf0b5a3cd6687ef25dd84a45
anjali790 <anjaliruby790@gmail.com>
2022-10-04 15:44:49 +0530

    useReducer

--- usereducer/src/components/ReducerCount.jsx
+++ usereducer/src/components/ReducerCount.jsx
@@ -2,32 +2,40 @@ import React, { useReducer } from "react";
 
 const initialState = {
     value: 0,
+    name: 'vasanth',
 }
 
 const reducer = (state, action) => {
     // console.log(action)
     switch (action.type) {
-        case 'increment':
+        case 'INCREMENT':
             return { ...state, value: state.value + action.value };
-        case 'decrement':
+        case 'DECREMENT':
             return { ...state, value: state.value - action.value };
-        case 'reset':
+        case 'RESET':
             return initialState;
+        case 'UPDATE':
+            return {...state, name: action.value};
         default:
             return state;
     }
 }
 
 export function ReducerCount() {
-    const [count, dispatch] = useReducer(reducer, initialState);
+    const [countObj, dispatch] = useReducer(reducer, initialState);
+    const [secondObj, secondDispatch] = useReducer(reducer, initialState);
 
     return (
         <>
-            <h1>count2 - {count.value}</h1>
-            <button onClick={()=> dispatch({type: 'increment', value: 10})}>Increment by 10</button>
-            <button onClick={() => dispatch({type: 'increment', value:5})}>Increment by 5</button>
-            <button onClick={() => dispatch({type: 'decrement', value:3})}>Decrement by 3</button>
-            <button onClick={() => dispatch({type: 'reset'})}>Reset</button>
+            <h1>count2 - {countObj.value}</h1>
+            <h2>name- {countObj.name}</h2>
+            <h3>count3 - {secondObj.value}</h3>
+            <button onClick={()=> dispatch({type: 'INCREMENT', value: 10})}>Increment by 10</button>
+            <button onClick={() => dispatch({type: 'INCREMENT', value:5})}>Increment by 5</button>
+            <button onClick={() => dispatch({type: 'DECREMENT', value:3})}>Decrement by 3</button>
+            <button onClick={()=> secondDispatch({type: 'DECREMENT', value:20})}>Drecrement secondObj by 20</button>
+            <button onClick={() => dispatch({type: 'RESET'})}>Reset</button>
+            <button onClick={()=> dispatch({type: 'UPDATE', value:'anjali'})}>change name</button>
         </>
     )
 }
