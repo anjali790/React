30489e1d7bf4452ec0bbcff54f74757fee12144a
anjali790 <anjaliruby790@gmail.com>
2022-10-06 15:42:33 +0530

    custom hooks

--- customhooks/src/components/CounterOne.jsx
+++ customhooks/src/components/CounterOne.jsx
@@ -1,23 +1,26 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
+import {useCounter} from './hooks/useCounter';
 
 export function CounterOne(){
-    const [counter, setCounter] = useState(0);
+    // const [counter, setCounter] = useState(0);
 
-    const increment = ()=>{
-        setCounter((prev)=>prev+1)
-    }
+    const [count, increment, decrement, reset] = useCounter(0);
 
-    const decrement = () =>{
-        setCounter((prev)=>prev-1)
-    }
+    // const increment = ()=>{
+    //     setCounter((prev)=>prev+1)
+    // }
 
-    const reset = () =>{
-        setCounter(0);
-    }
+    // const decrement = () =>{
+    //     setCounter((prev)=>prev-1)
+    // }
+
+    // const reset = () =>{
+    //     setCounter(0);
+    // }
 
     return(
         <>
-        <h1>counter one - {counter}</h1>
+        <h1>counter one - {count}</h1>
         <button onClick={increment}>Increment</button>
         <button onClick={decrement}>Decrement</button>
         <button onClick={reset}>Reset</button>
