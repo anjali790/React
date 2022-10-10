0f4c884235b8ff70a27688ae9625601b1e63a299
anjali790 <anjaliruby790@gmail.com>
2022-10-10 18:50:20 +0530

    router

--- router/src/App.js
+++ router/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import React from "react";
 import { Home } from './components/Home';
+import { Product } from './components/Product';
 import { About } from './components/About';
 import { Content } from './components/Content';
 import { Contact } from './components/Contact';
+import {Login} from './components/Login';
 import { Routes, Route } from 'react-router-dom';
 import { Header } from './components/Header';
+
 
 function App() {
   return (
@@ -13,9 +16,11 @@ function App() {
       <Header />
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/product' element={<Product/>}/>
         <Route path='/about' element={<About />} />
         <Route path='/about/:id' element={<Content />} />
         <Route path='/contact' element={<Contact />} />
+        <Route path='/login' element={<Login/>}/>
       </Routes>
     </>
   );
--- router/src/components/Header.jsx
+++ router/src/components/Header.jsx
@@ -13,7 +13,7 @@ export function Header() {
                         <nav>
                             <ul className='header_links'>
                                 <li><Link to='/' className='link'>Home</Link></li>
-                                <li ><Link to='/product' className='link'>Product</Link></li>
+                                <li><Link to='/product' className='link'>Product</Link></li>
                                 <li><Link to='/about' className='link'>About</Link></li>
                                 <li><Link to='/contact' className='link'>Contact</Link></li>
                                 <li><Link to='/login' className='link'>Login</Link></li>
