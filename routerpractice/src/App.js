fea16f0f1311351202ae18abd813b44e1fb33de2
anjali790 <anjaliruby790@gmail.com>
2022-10-14 20:43:23 +0530

    Router

---
+++ routerpractice/src/App.js
@@ -0,0 +1,47 @@
+import { Routes, Route } from 'react-router';
+import './App.css';
+import { About } from './components/About';
+import { Contact } from './components/Contact';
+// import { Contact2 } from './components/Contact2';
+import { Content } from './components/Content';
+import { Footer } from './components/Footer';
+import { Header } from './components/Header';
+import { Home } from './components/Home';
+import { Sidebar } from './components/Sidebar';
+import { Error } from './components/Error';
+
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path='*' element={<Sidebar/>}>
+        </Route>
+      </Routes>
+      {/* <Routes>
+        <Route path='/about' element={<Sidebar text="about" />}>
+        </Route>
+      </Routes>
+      <Routes>
+        <Route path='/contact' element={<Sidebar text="contact" />}>
+        </Route>
+      </Routes> */}
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+
+        <Route path='/about'>
+          <Route index element={<About />} />
+          <Route path=':id' element={<Content />}></Route>
+        </Route>
+
+        <Route path='/contact' element={<Contact />} />
+        <Route path='*' element={<Error />}></Route>
+      </Routes>
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
