4fa5a2a0fcb89b48a6c835bdd7e7d85ccf6f2fb1
anjali790 <anjaliruby790@gmail.com>
2022-10-14 13:03:32 +0530

    Router

---
+++ routerpractice/src/components/About.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+// import { Link } from 'react-router-dom';
+
+export function About() {
+    return (
+        <>
+            <div className='about'>About
+                {/* <p>
+                    <Link to='/about1'>Content1</Link>
+                    <Link to='/about2'>Content2</Link>
+                </p> */}
+            </div>
+        </>
+    )
+}
