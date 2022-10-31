565584cb267121d3b296263e8fe290eba5934b94
anjali790 <anjaliruby790@gmail.com>
2022-10-31 23:21:20 +0530

    usereducer

--- usereducer/src/components/ReducerCount.jsx
+++ usereducer/src/components/ReducerCount.jsx
@@ -15,7 +15,7 @@ const reducer = (state, action) => {
         case 'RESET':
             return initialState;
         case 'UPDATE':
-            return {...state, name: action.value};
+            return { ...state, name: action.value };
         default:
             return state;
     }
@@ -30,13 +30,13 @@ export function ReducerCount() {
             <h1>count2 - {countObj.value}</h1>
             <h2>name- {countObj.name}</h2>
             <h3>count3 - {secondObj.value}</h3>
-            <button onClick={()=> dispatch({type: 'INCREMENT', value: 10})}>Increment by 10</button>
-            <button onClick={() => dispatch({type: 'INCREMENT', value:5})}>Increment by 5</button>
-            <button onClick={() => dispatch({type: 'DECREMENT', value:3})}>Decrement by 3</button>
-            <button onClick={()=> secondDispatch({type: 'DECREMENT', value:20})}>Drecrement secondObj by 20</button>
-            <button onClick={() => dispatch({type: 'RESET'})}>Reset</button>
-            <button onClick={()=> secondDispatch({type: 'RESET'})}>Reset Second</button>
-            <button onClick={()=> dispatch({type: 'UPDATE', value:'anjali'})}>change name</button>
+            <button onClick={() => dispatch({ type: 'INCREMENT', value: 10 })}>Increment by 10</button>
+            <button onClick={() => dispatch({ type: 'INCREMENT', value: 5 })}>Increment by 5</button>
+            <button onClick={() => dispatch({ type: 'DECREMENT', value: 3 })}>Decrement by 3</button>
+            <button onClick={() => secondDispatch({ type: 'DECREMENT', value: 20 })}>Drecrement secondObj by 20</button>
+            <button onClick={() => dispatch({ type: 'RESET' })}>Reset</button>
+            <button onClick={() => secondDispatch({ type: 'RESET' })}>Reset Second</button>
+            <button onClick={() => dispatch({ type: 'UPDATE', value: 'anjali' })}>change name</button>
         </>
     )
 }
