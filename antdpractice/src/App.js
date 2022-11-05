e11f10eb67e94d11eb262727d93175195313c05e
anjali790 <anjaliruby790@gmail.com>
2022-11-05 23:46:28 +0530

    antd practice

---
+++ antdpractice/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import 'antd/dist/antd.css';
+import { Buttons } from './components/Buttons';
+
+function App() {
+  return (
+    <>
+      <Buttons />
+    </>
+  );
+}
+
+export default App;
---
+++ antdpractice/src/components/Buttons.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Button } from 'antd';
+
+export function Buttons() {
+    return (
+        <>
+            <Button type="primary">click me</Button>
+            <Button type="dashed">Dashed Button</Button>
+        </>
+    )
+}
