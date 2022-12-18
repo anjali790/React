3b1694a3533e1a067f9b3a55feb598bfc8eca3cd
anjali790 <anjaliruby790@gmail.com>
2022-12-18 23:59:37 +0530

    usememo

--- usememo/src/components/MemoCounter.jsx
+++ usememo/src/components/MemoCounter.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useMemo } from "react";
 
 export function MemoCounter() {
     const [counterOne, setCounterOne] = useState(0);
@@ -17,30 +17,29 @@ export function MemoCounter() {
         setCounterThree(counterThree + 1);
     }
 
-    // const isEven = useMemo(() => {
-    //     console.log('even fn')
-    //     let i = 0;
-    //     while (i < 1000000) i++;
-    //     return counterOne % 2 === 0;
-    // },[counterOne])
-
-    const isEven = () => {
-        // console.log('even fn')
+    const isEven = useMemo(() => {
+        console.log('even fn')
         let i = 0;
         while (i < 2000000000) i++;
         return counterOne % 2 === 0;
-    };
+    }, [counterOne])
+
+    // const isEven = () => {
+    //     // console.log('even fn')
+    //     let i = 0;
+    //     while (i < 2000000000) i++;
+    //     return counterOne % 2 === 0;
+    // };
 
     return (
         <>
-            {/* <h1>counterOne- {counterOne} -{isEven() ? 'EVEN' : 'ODD'}</h1> */}
-            <h1>counterOne- {counterOne} - {isEven() ? 'EVEN' : 'ODD'}</h1>
+            <h1>counterOne- {counterOne} -{isEven ? 'EVEN' : 'ODD'}</h1>
+            {/* <h1>counterOne- {counterOne} - {isEven() ? 'EVEN' : 'ODD'}</h1> */}
             <h1>counterTwo- {counterTwo}</h1>
             <h1>counterThree- {counterThree}</h1>
             <button onClick={incrementOne}>increment CounterOne</button>
             <button onClick={incrementTwo}>increment CounterTwo</button>
             <button onClick={incrementThree}>increment CounterThree</button>
-
         </>
     )
 }
