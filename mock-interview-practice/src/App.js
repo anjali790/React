ae9fc75cce22f26a661136da8b2f10db7fe6affb
anjali790 <anjaliruby790@gmail.com>
2022-12-26 08:01:17 +0530

    practice

--- mock-interview-practice/src/App.js
+++ mock-interview-practice/src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
-import { Timer } from './components/Timer';
 import { AppendCounter } from './components/AppendCounter';
 import { Counter } from './components/Counter';
+import { DigitalClock } from './components/DigitalClock';
 
 function App() {
   return (
     <div className="App">
       <Counter />
       <AppendCounter />
-      <Timer />
+      <DigitalClock />
     </div>
   );
 }
--- mock-interview-practice/src/components/DigitalClock.jsx
+++ mock-interview-practice/src/components/DigitalClock.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 import { useState } from 'react';
 
 let currentTime = new Date().toLocalTimeString
+// let currentTime = new Date().toLocaleTimeString()
 
-export function Timer() {
+export function DigitalClock() {
     const [time, setTime] = useState(currentTime);
 
     setInterval(() => {
