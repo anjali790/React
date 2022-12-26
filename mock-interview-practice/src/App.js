18442fc3e2500e11670e470c355d02b81287e50f
anjali790 <anjaliruby790@gmail.com>
2022-12-26 07:51:33 +0530

    practice

--- mock-interview-practice/src/App.js
+++ mock-interview-practice/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import { Timer } from './components/Timer';
+import { AppendCounter } from './components/AppendCounter';
 import { Counter } from './components/Counter';
 
 function App() {
   return (
     <div className="App">
-      <Counter/>
+      <Counter />
+      <AppendCounter />
+      <Timer />
     </div>
   );
 }
