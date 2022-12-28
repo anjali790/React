5a226daf76e22f6233a0bb322d8cec920fbda784
anjali790 <anjaliruby790@gmail.com>
2022-12-28 19:04:46 +0530

    practice

--- error-boundary/src/components/UIBackup.jsx
+++ error-boundary/src/components/UIBackup.jsx
@@ -8,16 +8,11 @@ export class UIBackup extends Component {
         }
     }
 
-    // static getDerivedStateFromError() {
-    //     return {
-    //         hasError: true,
-    //     }
-    // }
-
-    componentDidCatch(error, errormsg) {
-        // You can also log the error to an error reporting service
-        console.log(error, "something went wrong comp did catch error");
-      }
+    static getDerivedStateFromError() {
+        return {
+            hasError: true,
+        }
+    }
 
     render() {
         if (this.state.hasError) {
