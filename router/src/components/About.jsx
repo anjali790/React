a98468761a8fb588a6f6f5aec901a72acc438785
anjali790 <anjaliruby790@gmail.com>
2023-01-11 03:42:16 +0530

    practice

--- router/src/components/About.jsx
+++ router/src/components/About.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export function About() {
   return (
     <>
-    <h1>About</h1>
-    <p>
-      <Link to='/about/1'>Content 1</Link>
-      <Link to='/about/2'>Content 2</Link>
-    </p>
+      <h1>About</h1>
+      <p>
+        <Link to='/about/1'>Content 1</Link>
+        <Link to='/about/2'>Content 2</Link>
+      </p>
     </>
   )
 }
