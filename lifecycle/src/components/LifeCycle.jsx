92b1e6f8a287cde1bacbc4ab1e687565ed70917f
anjali790 <anjaliruby790@gmail.com>
2023-01-11 03:43:18 +0530

    practice

--- lifecycle/src/components/LifeCycle.jsx
+++ lifecycle/src/components/LifeCycle.jsx
@@ -1,10 +1,10 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
-export class LifeCycle extends Component{
-    constructor(props){
+export class LifeCycle extends Component {
+    constructor(props) {
         super(props);
         this.state = {
-            count : 0,
+            count: 0,
         };
 
         console.log('initialisation');// it will call only once
@@ -12,54 +12,54 @@ export class LifeCycle extends Component{
 
     incrementCount = () => {
         this.setState((prev) => ({
-            count: prev.count +1,
+            count: prev.count + 1,
         }));
     }
-    
-    
+
+
 
     decrementCount = () => {
-        this.setState((prev) =>({
-            count: prev.count -1,
+        this.setState((prev) => ({
+            count: prev.count - 1,
         }));
     }
 
-    componentWillMount(){
+    componentWillMount() {
         console.log('component will mount');
     }
 
-    componentDidMount(){
+    componentDidMount() {
         console.log('component did mount');
     }
-    
-    shouldComponentUpdate(){
+
+    shouldComponentUpdate() {
         console.log('should component update');
         return true;
     }
 
-    componentWillUpdate(){
+    componentWillUpdate() {
         console.log('component will update');
     }
 
-    componentDidUpdate(){
+    componentDidUpdate() {
         console.log('component did update');
     }
 
-    componentWillUnmount(){
+    componentWillUnmount() {
         console.log('component will unmount');
         alert('in unmounting phase');
     }
 
-    render(){
-    //    this.setState({count: this.state.count+1 })-------->>>WRONG -> NEVER EVER CHANGE THE STATE INSIDE RENDER METHOD THIS WILL MAKE A INFINTE LOOP
-     
+    render() {
+        //    this.setState({count: this.state.count+1 })-------->>>WRONG -> NEVER EVER CHANGE THE STATE INSIDE RENDER METHOD THIS WILL MAKE A INFINTE LOOP
+
         console.log('rendering');
-        return(
+        return (
             <>
-            <h1>count- {this.state.count}</h1>
-            <button onClick={this.incrementCount}>increment</button>
-            <button onClick={this.decrementCount}>decrement</button>
-            {this.state.count > 10 && <h1>hello world</h1>}
+                <h1>count- {this.state.count}</h1>
+                <button onClick={this.incrementCount}>increment</button>
+                <button onClick={this.decrementCount}>decrement</button>
+                {this.state.count > 10 && <h1>hello world</h1>}
             </>
         )
     }
